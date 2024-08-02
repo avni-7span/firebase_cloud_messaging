a3f933abd301fe0551410018391009be94f652d8
Avni Prajapati <avniprajapati@jill.local>
2024-08-02 16:45:11 +0530

    background notification implementation

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,46 +1,24 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:push_notification/main.dart';
 
+void handleMessage(RemoteMessage? message) {
+  if (message == null) return;
+  navigatorKey.currentState
+      ?.pushNamed('/notification_screen', arguments: message);
+}
+
 class FirebaseApi {
   final instance = FirebaseMessaging.instance;
 
-  /// function to initialize notification
   Future<void> initNotification() async {
-    /// request permission from user
     await instance.requestPermission();
     await initPushNotifications();
-
-    /// fetch the FCM(firebase cloud messaging) token for this device
     final fcmToken = await instance.getToken();
-
-    /// send token to server
     print('Token : $fcmToken');
   }
 
-  /// handling received messages and user taps notifications
-  void handleMessage(RemoteMessage? message) {
-    if (message == null) return;
-    navigatorKey.currentState
-        ?.pushNamed('/notification_screen', arguments: message);
-  }
-
-  /// function to initialize background settings
   Future<void> initPushNotifications() async {
-    /// attach event listeners for when tapping on notification opens the app
-    /// background state
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
-
-    /// handle notification if the app was terminated and now open
-    /// terminated state
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
   }
 }
-
-/// - APNSToken is the Apple Push Notification Service token.
-/// It is a token (think of it like a password) that authenticates your app and
-/// device onto the Apple Push service and allows for communications to be sent.
-
-/// - FCMToken is the Firebase Cloud Messaging token.
-/// This is googles version of the APNS Token however works for both iOS and
-/// Android (Google do proxying on their end when sending a
-/// push notification to iOS devices).
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:push_notification/api/firebase_api.dart';
-import 'package:push_notification/firebase_options.dart';
 import 'package:push_notification/screens/home_screen.dart';
 import 'package:push_notification/screens/notification_screen.dart';
 
 /// NavigatorState : key to find screen , Global : can be used anywhere in app
 final navigatorKey = GlobalKey<NavigatorState>();
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await FirebaseApi().initNotification();
   runApp(const MyApp());
 }
